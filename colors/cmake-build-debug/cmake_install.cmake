36ffb58c4897a86ad45a8502be802c1c4164ba0b
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-12-18 15:21:53 +0200

    updated src code

--- colors/cmake-build-debug/cmake_install.cmake
+++ colors/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ozil/CLionProjects/colors
+# Install script for directory: /home/ozil/GitHub/RANDOM/colors
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ozil/CLionProjects/colors/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ozil/GitHub/RANDOM/colors/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- colors/main.cpp
+++ colors/main.cpp
@@ -31,6 +31,21 @@ int main() {
     /* Background Colors */
     cout<<BLACK<<"┃"<<RESET_COLOR<<BACKGROUND_BLACK<<" 00xWolf "<<BACKGROUND_RED<<"00xWolf "<<BACKGROUND_GREEN<<"00xWolf "<<BACKGROUND_YELLOW<<"00xWolf "<<BACKGROUND_BLUE<<"00xWolf "<<BACKGROUND_PURPLE<<"00xWolf "<<BACKGROUND_CYAN<<"00xWolf "<<BACKGROUND_WHITE<<"00xWolf "<<BLACK<<"┃"<<RESET_COLOR<<endl;
 
+    cout<<BLACK<<"┃                                                                 ┃"<<RESET_COLOR<<endl;
+
+    /* High Intensity Colors */
+    cout<<BLACK<<"┃"<<RESET_COLOR<<HIGH_INTENSITY_BLACK<<" 00xWolf "<<HIGH_INTENSITY_RED<<"00xWolf "<<HIGH_INTENSITY_GREEN<<"00xWolf "<<HIGH_INTENSITY_YELLOW<<"00xWolf "<<HIGH_INTENSITY_BLUE<<"00xWolf "<<HIGH_INTENSITY_PURPLE<<"00xWolf "<<HIGH_INTENSITY_CYAN<<"00xWolf "<<HIGH_INTENSITY_WHITE<<"00xWolf "<<BLACK<<"┃"<<RESET_COLOR<<endl;
+
+    cout<<BLACK<<"┃                                                                 ┃"<<RESET_COLOR<<endl;
+
+    /* Bold High Intensity Colors */
+    cout<<BLACK<<"┃"<<RESET_COLOR<<BOLD_HIGH_INTENSITY_BLACK<<" 00xWolf "<<BOLD_HIGH_INTENSITY_RED<<"00xWolf "<<BOLD_HIGH_INTENSITY_GREEN<<"00xWolf "<<BOLD_HIGH_INTENSITY_YELLOW<<"00xWolf "<<BOLD_HIGH_INTENSITY_BLUE<<"00xWolf "<<BOLD_HIGH_INTENSITY_PURPLE<<"00xWolf "<<BOLD_HIGH_INTENSITY_CYAN<<"00xWolf "<<BOLD_HIGH_INTENSITY_WHITE<<"00xWolf "<<BLACK<<"┃"<<RESET_COLOR<<endl;
+
+    cout<<BLACK<<"┃                                                                 ┃"<<RESET_COLOR<<endl;
+
+    /* High Intensity Background Colors */
+    cout<<BLACK<<"┃"<<RESET_COLOR<<HIGH_INTENSITY_BACKGROUND_BLACK<<" 00xWolf "<<HIGH_INTENSITY_BACKGROUND_RED<<"00xWolf "<<HIGH_INTENSITY_BACKGROUND_GREEN<<"00xWolf "<<HIGH_INTENSITY_BACKGROUND_YELLOW<<"00xWolf "<<HIGH_INTENSITY_BACKGROUND_BLUE<<"00xWolf "<<HIGH_INTENSITY_BACKGROUND_PURPLE<<"00xWolf "<<HIGH_INTENSITY_BACKGROUND_CYAN<<"00xWolf "<<HIGH_INTENSITY_BACKGROUND_WHITE<<"00xWolf "<<BLACK<<"┃"<<RESET_COLOR<<endl;
+
     cout<<BLACK<<"┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"<<RESET_COLOR<<endl;
 
     return 0;
